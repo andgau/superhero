79d20ec1b3d244459a5999efc09aad760f990a51
Andres <andres-gaudioso@outlook.es>
2021-07-24 19:07:37 +0200

    Completadas las operaciones básicas

--- src/main/java/es/sinjava/superhero/controllers/SuperheroController.java
+++ src/main/java/es/sinjava/superhero/controllers/SuperheroController.java
@@ -3,8 +3,12 @@ package es.sinjava.superhero.controllers;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.sinjava.superhero.beans.SuperheroBean;
@@ -21,15 +25,37 @@ public class SuperheroController {
 
 	@GetMapping("/superhero/{id}")
 	public ResponseEntity<?> byId(@PathVariable Long id) {
-		log.debug("TracedItemController {} byId", id);
+		log.debug("SuperHeroController {} byId", id);
 		Optional<SuperheroBean> item = superHeroService.findById(id);
 		return ResponseEntity.of(item);
 	}
 
 	@GetMapping("/superhero")
 	public ResponseEntity<?> all() {
+		log.debug("SuperHeroController All");
 		Iterable<SuperheroBean> item = superHeroService.findAll();
 		return ResponseEntity.ok(item);
+	}
+
+	@PostMapping("/superhero")
+	public ResponseEntity<?> create(@RequestBody SuperheroBean superhero) {
+		log.debug("SuperHeroController create");
+		SuperheroBean item = superHeroService.save(superhero);
+		return ResponseEntity.ok(item);
+	}
+
+	@PutMapping("/superhero")
+	public ResponseEntity<?> update(@RequestBody SuperheroBean superhero) {
+		log.debug("SuperHeroController update");
+		SuperheroBean item = superHeroService.edit(superhero);
+		return ResponseEntity.ok(item);
+	}
+
+	@DeleteMapping("/superhero/{id}")
+	public ResponseEntity<?> deleteById(@PathVariable Long id) {
+		log.debug("SuperHeroController {} byId", id);
+		superHeroService.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
