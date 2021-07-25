b5eb79be92b973aa50fb241ce31ede5ce8bc65bf
Andres <andres-gaudioso@outlook.es>
2021-07-25 10:51:17 +0200

    Revisión de las llamadas

--- src/main/java/es/sinjava/superhero/services/BaseServiceImpl.java
+++ src/main/java/es/sinjava/superhero/services/BaseServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
+import es.sinjava.superhero.audit.SmartAudit;
 import lombok.extern.log4j.Log4j2;
 
 // Clase que elimina código repetivo en los servicios
@@ -16,36 +17,42 @@ public abstract class BaseServiceImpl<T, ID, R extends CrudRepository<T, ID>> im
 	protected R repo;
 
 	@Override
+	@SmartAudit
 	public T save(T t) {
 		log.debug("Save entity  {} ", t.getClass().getName());
 		return repo.save(t);
 	}
 
 	@Override
+	@SmartAudit
 	public Optional<T> findById(ID id) {
 		log.debug("Find entity  {} ", id);
 		return repo.findById(id);
 	}
 
 	@Override
+	@SmartAudit
 	public Iterable<T> findAll() {
 		log.debug("Find All ");
 		return repo.findAll();
 	}
 
 	@Override
+	@SmartAudit
 	public T edit(T t) {
 		log.debug("Update entity  {}", t.getClass().getName());
 		return repo.save(t);
 	}
 
 	@Override
+	@SmartAudit
 	public void delete(T t) {
 		log.debug("Delete entity  {}", t.getClass());
 		repo.delete(t);
 	}
 
 	@Override
+	@SmartAudit
 	public void deleteById(ID id) {
 		log.debug("Delete entity by Id  {}", id);
 		if (repo.existsById(id)) {
